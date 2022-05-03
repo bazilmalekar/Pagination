6391f4cc915c7a9a777872b067852de6cdf55126
98971224733 <bazilmalekar8@gmail.com>
2022-05-03 17:34:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Mock_Data from "./MOCK_DATA.json";
+import ReactPaginate from 'react-paginate';
 
 function App() {
+  const [users, setUsers] = useState(Mock_Data.slice(0, 50));
+  const [pageNumber, setPageNumber] = useState(0);
+  const userPerPage = 10;
+  const pageVisited = pageNumber * userPerPage;
+  const pageCount = Math.ceil(users.length / userPerPage);
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users
+        .slice(pageVisited, pageVisited + userPerPage)
+        .map((elem) => {
+          return(
+            <div className="user_info" key={elem.id}>
+              <p>First name: {elem.first_name}</p>
+              <p>Last name: {elem.last_name}</p>
+              <p>Email: {elem.email}</p>
+            </div>
+          );
+        })
+      }    
+      <ReactPaginate
+      previousLabel={"<"}
+      nextLabel={">"}
+      pageCount={pageCount}
+      onPageChange={changePage}
+      containerClassName={"pagination_container"}
+      nextClassName={"pagination_next"}
+      previousClassName={"pagination_previous"}
+      activeClassName={"paginaiton_active"}
+      disabledClassName={"pagination_disable"}
+      />  
     </div>
   );
 }
